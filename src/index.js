c481665c2fc4e9749e3d268cea22b6e936ec7865
Ivan Luis Leiva Garcia <ileivag@proveedor.intercorp.com.pe>
2022-05-06 17:10:57 -0500

    fetching movies implement

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+import fetchWithTimeout from './services.js';
+let movies = require('./data/movies.json');
+
 export function fetchMovies() {
+    const resolveFunction = () => { return movies };
+    return fetchWithTimeout(1000).then(resolveFunction);
 }
+
+let moviePromise = fetchMovies();
+moviePromise.then(results => {
+    console.log(results);
+});
