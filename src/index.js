628c99b66db6e45810971dd7734a308b33f99870
Ivan Luis Leiva Garcia <ileivag@proveedor.intercorp.com.pe>
2022-05-07 18:06:37 -0500

    promise all, promise race implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import { fetchWithTimeout } from "./services.js";
+import { fetchMovies, fetchBooks } from "./services.js";
 let movies = require("./data/movies.json");
 
-export function fetchMovies() {
-    const resolveFunction = () => movies;
-    return fetchWithTimeout(1000).then(resolveFunction);
+function getBooksAndMovies() {
+    return Promise.all([fetchBooks(), fetchMovies()])
+        .then(([books, movies]) => ({
+            books,
+            movies
+        }))
+        .catch(error => console.log("Error fetching books and movies", error));
 }
 
-const moviePromise = fetchMovies();
-moviePromise.then(results => {
-    console.log(results);
-});
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then(results => console.log('getBooksAndMoviesPromise', results));
+
+function getBooksOrMovies() {
+    return Promise.race([fetchBooks(), fetchMovies()])
+        .then(results => results)
+        .catch(error => console.log("Error waiting for the promise race", error));
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then(results => console.log('getBooksOrMoviesPromise', results));
