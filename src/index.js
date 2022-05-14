ea09d7f824f797fa5a21b88f58563103fa7f0832
Ivan Luis Leiva Garcia <ileivag@proveedor.intercorp.com.pe>
2022-05-13 19:57:35 -0500

    async await impl

--- src/index.js
+++ src/index.js
@@ -31,24 +31,33 @@ async function getBooksAndMoviesAsync() {
         return { books, movies };
     } catch (error) {
         console.error("Error fetching books and movies", error);
+        return error;
     }
 }
 
 async function getBooksOrMoviesAsync() {
-    try {
-        const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
-        return values;
-    } catch (error) {
-        console.error("Error waiting for the promise race", error);
-    }
+    const values = await Promise.race([asyncFetchBookss(), asyncFetchMovies()]);
+    return values;
 }
 
 getBooksAndMoviesAsync()
     .then(results => {
         console.log("movies and books", { movies: results.movies, books: results.books });
+    })
+    .catch(error => {
+        console.error("Error in getBooksAndMoviesAsync execution", error);
     });
 
 getBooksOrMoviesAsync()
     .then(results => {
         console.log("movies or books", { results });
     });
+
+const timer1 = setTimeout(() => {
+    console.log("timer 1 has finished");
+}, 3000);
+
+const time2 = setTimeout(() => {
+    console.log("timer 2 has finished");
+    clearTimeout(timer1);
+}, 2000);
